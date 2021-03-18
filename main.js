6b342ff25538d5540bbdd84c3f27c1a080ff93e9
mhewedy <mhewedy@elm.sa>
2021-03-18 19:33:16 +0300

    init

---
+++ main.js
@@ -0,0 +1,26 @@
+(function () {
+    let snd = new Audio(chrome.runtime.getURL("alarm.mp3"));
+
+
+    function foo() {
+        let a = document.querySelector('#content_wrapper > div.col-xs-12.col-sm-12.col-md-12.col-lg-12.col-xl-12 > div.col-md-12 > div > div > div > div.panel.panel-success > div.panel-body > p > a');
+
+        if (a) {
+            let url = a.href;
+
+            if (sessionStorage.getItem(url)) {
+                return;
+            }
+
+            window.open(url, '_blank');
+            for (i = 0; i < 10; i++) {
+                snd.play();
+            }
+            sessionStorage.setItem(url, 'true');
+        }
+
+        setTimeout(foo, 5000);
+    }
+
+    foo();
+}());
