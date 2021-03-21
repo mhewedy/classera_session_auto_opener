3dd16f1111e5eae8798067d29d887e6ee29ecc72
mhewedy <mhewedy@gmail.com>
2021-03-21 23:13:32 +0300

    fix the classera does not reload by itself

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
 (function () {
     let snd = new Audio(chrome.runtime.getURL("alarm.mp3"));
 
+    function openClass() {
 
-    function foo() {
-        let a = document.querySelector('#content_wrapper > div.col-xs-12.col-sm-12.col-md-12.col-lg-12.col-xl-12 > div.col-md-12 > div > div > div > div.panel.panel-success > div.panel-body > p > a');
+        let timeDiv = document.querySelectorAll('#content_wrapper > div.col-xs-12.col-sm-12.col-md-12.col-lg-12.col-xl-12 > div.col-md-12 > div > div > div > div > div.panel-body > p:nth-child(3)');
+        
+        let selectedDiv = [...timeDiv].find(it => {
+            let hour = parseInt(it.textContent.split(':')[0], 10)
+            let min = parseInt(it.textContent.split(':')[1], 10)
+            return hour == new Date().getHours() && min == new Date().getMinutes();
+        });
 
-        if (a) {
-            let url = a.href;
-
-            if (sessionStorage.getItem(url)) {
+        if (selectedDiv) {
+            let urlKey = 'session_opened_' + new Date().toJSON().slice(0,10).replace(/-/g,'-') + 'T' + selectedDiv.textContent;
+            
+            if (sessionStorage.getItem(urlKey)) {
                 return;
+            }else {
+                let teamsUrl = selectedDiv.parentElement.children[3].children[0].href;
+
+                let linkNotFound = teamsUrl.includes("#");
+                if (linkNotFound) {
+                    window.location.reload();
+                } else {
+                    sessionStorage.setItem(urlKey, 'true');
+
+                    window.open(teamsUrl, '_blank');
+                    snd.play();
+                }
             }
-
-            window.open(url, '_blank');
-            snd.play();
-           
-            sessionStorage.setItem(url, 'true');
         }
-
-        setTimeout(foo, 5000);
+        setTimeout(openClass, 5000);
     }
 
-    foo();
+    openClass();
 }());
